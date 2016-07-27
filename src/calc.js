923c019a6ab3aeb089e00cb765606223408f0d89
Aravind M <murali.aravind94@gmail.com>
2016-07-27 12:23:40 +0530

    Add input Validation Add Process Input Refactor getnum to include above methods

--- src/calc.js
+++ src/calc.js
@@ -14,9 +14,39 @@ var calculator = {
 };
 
 function getnum() {
-  operand1 = parseInt(document.forms["myform"]["num1"].value);
-  operand2 = parseInt(document.forms["myform"]["num2"].value);
-  operator = document.forms["myform"]["operator"].value;
-  num = calculator[operator](operand1,operand2);
-  document.getElementById("result").innerHTML = num;
+  object = processInput();
+  if (validateInput(object)){
+    operand1 = parseInt(object.number1);
+    operand2 = parseInt(object.number2);
+    operator = document.forms["myform"]["operator"].value;
+    num = calculator[operator](operand1,operand2);
+    document.getElementById("result").innerHTML = num;
+  }
+}
+
+function processInput() {
+  op1 = document.forms["myform"]["num1"].value;
+  op2 = document.forms["myform"]["num2"].value;
+  return {
+    number1: op1,
+    number2: op2
+  };
+}
+
+
+function validateInput(object) {
+  var numbers = /^[0-9]+$/;
+  if (object.number1 == '' || object.number2 == '') {
+    alert('Enter values for number');
+    document.forms["myform"].focus();
+    return false;
+  }
+  else if(object.number1.match(numbers) && object.number2.match(numbers) ) {
+      return true;
+  }
+  else {
+    alert('Enter numeric values : ');
+    document.forms["myform"].focus();
+    return false;
+  }
 }
